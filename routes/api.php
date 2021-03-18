b89cdb93c8de5eea1ae3152e6d385731fe744b05
marencoalf <alfredomarenco@boletea.com>
2021-03-18 00:51:03 -0600

    Commit para crear webhook

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::any('/mywebhook', function () {
     $content    =   file_get_contents("php://input");
-    $respuesta  =   json_decode($content);
+    $respuesta  =   json_encode($content);
     Log::info($respuesta);
     return response()->json(200);
 });
