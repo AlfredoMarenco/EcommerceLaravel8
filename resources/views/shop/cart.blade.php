bdb32f8b34a1e5db68f92b33c243feb3e35a1425
marencoalf <alfredomarenco@boletea.com>
2021-03-28 21:29:38 -0600

    cambios del fronte para pasar a servidor de desarrollo

--- resources/views/shop/cart.blade.php
+++ resources/views/shop/cart.blade.php
@@ -149,7 +149,7 @@
                                 <div class="card-header" id="headingOne">
                                     <h2 class="mb-0">
                                         <button class="btn btn-link btn-block text-left" type="button"
-                                            data-toggle="collapse" data-target="#collapseOne" aria-expanded="true"
+                                            data-toggle="collapse" data-target="#collapseOne" aria-expanded="false"
                                             aria-controls="collapseOne">
                                             Pago con tarjeta
                                         </button>
