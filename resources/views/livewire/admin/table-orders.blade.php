ab9c4192e9eccf93ec143cbf52575f8b83abb2ac
marencoalf <alfredomarenco@boletea.com>
2021-03-28 22:26:18 -0600

    cambios del fronte para pasar a servidor de desarrollo

--- resources/views/livewire/admin/table-orders.blade.php
+++ resources/views/livewire/admin/table-orders.blade.php
@@ -2,7 +2,7 @@
     <div class="card">
         <div class="card-body">
             <div class="d-flex flex-row justify-content-start">
-                <input class="form-control col-3" wire:model="search" type="text" placeholder="Busqueda de posts">
+                <input class="form-control col-3" wire:model="search" type="text" placeholder="Busqueda del cliente">
             </div>
             <div class="d-flex flex-row-reverse justify-content-between my-3">
                 <div>
