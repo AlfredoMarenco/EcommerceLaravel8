183d96dd2303f7724e6aa3aa84e9496a1b512319
marencoalf <alfredomarenco@boletea.com>
2021-03-31 13:12:17 -0600

    Mail de creacion de ordenes

--- resources/views/emails/orders/shipped.blade.php
+++ resources/views/emails/orders/shipped.blade.php
@@ -1,7 +1,17 @@
 @component('mail::message')
+
 # Tu numero de orden es: {{ $order->id }}
 
-Tu orden ha sido creada
+Total: <h3>${{ number_format($order->amount,2) }}</h3>
+
+@component('mail::table')
+| Quanty       | Product         | Price  |
+| ------------- |:-------------:| --------:|
+@foreach ($order->products as $product)
+| {{ $product->pivot->quanty  }}      | {{ $product->name }}      | ${{ number_format($product->pivot->price, 2) }}      |
+@endforeach
+
+@endcomponent
 
 @component('mail::button', ['url' => 'https://renealonso.com/user/orders','color' => 'success'])
 Ver orden
