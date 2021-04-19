7bccb16b1ecc347a5677e0d5af75cea6b0746ed4
marencoalf <alfredomarenco@boletea.com>
2021-04-19 14:16:14 -0500

    Cambios solicitados por el pdf

--- resources/views/shop/cart.blade.php
+++ resources/views/shop/cart.blade.php
@@ -221,20 +221,6 @@
                 </div>
             </div> <!-- container .//  -->
         </section>
-
-        <!-- ========================= SECTION  ========================= -->
-        <section class="section-name border-top padding-y">
-            <div class="container">
-                <h6>Payment and refund policy</h6>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div><!-- container // -->
-        </section>
-        <!-- ========================= SECTION  END// ========================= -->
     @else
         <section class="section-content padding-y">
             <div class="container mb-3 mt-5 py-3">
