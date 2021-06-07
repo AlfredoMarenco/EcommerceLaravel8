44a2209974c4404c04526a89363c48a37d0b73ec
marencoalf <alfredomarenco@boletea.com>
2021-06-07 10:23:19 -0500

    Agregamos boton para imagen del cupon

--- resources/views/bajce/index.blade.php
+++ resources/views/bajce/index.blade.php
@@ -275,7 +275,7 @@
 
     <section id="codigo" class="p-0">
         @foreach ($cuponfs as $cuponf)
-            <div style="background-image: url({{ Storage::url($cuponf->image->url) }}); background-position: center;  background-size: 100%; background-repeat: no-repeat; min-height:400px;">
+            <div style="background-image: url({{ Storage::url($cuponf->image->url) }}); background-position: center;  background-size: 100%; background-repeat: no-repeat; min-height:450px;">
                 <div class="contenido">
                     <h2>
                         {{ $cuponf->text }}
