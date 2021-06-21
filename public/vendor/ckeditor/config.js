433f1e8a165a6eddd99259e5963dc71b1e9c8479
marencoalf <alfredomarenco@boletea.com>
2021-06-21 17:42:01 -0500

    Cambios para preparar todo la para la implementacion

--- public/vendor/ckeditor/config.js
+++ public/vendor/ckeditor/config.js
@@ -5,7 +5,7 @@
 
 CKEDITOR.editorConfig = function(config) {
     // Define changes to default configuration here. For example:
-    config.language = 'es';
+    /* config.language = 'es'; */
     // config.uiColor = '#AADC6E';
     config.extraPlugins = 'bootstraptable';
 };
