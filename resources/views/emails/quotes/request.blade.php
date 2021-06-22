5af306f8f8aaa016741e9a75522b98251ee7af9a
marencoalf <alfredomarenco@boletea.com>
2021-06-22 16:42:13 -0500

    Modificacion en el mail de las cotizaciones

--- resources/views/emails/quotes/request.blade.php
+++ resources/views/emails/quotes/request.blade.php
@@ -7,7 +7,7 @@ Commentario: {{ $comment }}
 @component('mail::table')
 @foreach (Cart::instance('wishlist')->content() as $product)
 | Nombre | Cantidad | SKU |
-| ------------- | --------:| ----------:|
+| ------------- | --------:| ------------:|
 | {{ $product->model->name }} | {{ $product->qty }} | {{ $product->model->SKU }}  |
 @endforeach
 @endcomponent
