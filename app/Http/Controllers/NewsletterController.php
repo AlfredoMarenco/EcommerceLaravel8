2b77d3761e7296142fd3804ba260c4fbadb0e9df
marencoalf <alfredomarenco@boletea.com>
2021-06-23 21:59:39 -0500

    Creamos la funcion del newsletter

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -14,6 +14,6 @@ class NewsletterController extends Controller
             'email' => $request->email,
         ]);
 
-        return back();
+        return back()->withSuccess('Subscripcion completada');
     }
 }
