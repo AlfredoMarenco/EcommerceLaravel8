971552679d04397e0ee4eb3b33e2b049e4f62822
marencoalf <alfredomarenco@boletea.com>
2021-07-02 09:40:47 -0500

    Restauramos valores ya que no fue solcion para desfase del carrito

--- resources/views/bajce/shop/shopping-cart.blade.php
+++ resources/views/bajce/shop/shopping-cart.blade.php
@@ -9,14 +9,14 @@
             <div class="row">
                 @if (Cart::instance('wishlist')->count() + Cart::instance('default')->count() > 0)
                     <main class="col-md-9">
-                        <div class="card table-responsive">
-                            <table class="table table-borderless table-shopping-cart table-sm">
+                        <div class="card">
+                            <table class="table table-borderless table-shopping-cart">
                                 <thead class="text-muted">
                                     <tr class="small text-uppercase">
                                         <th scope="col">Producto</th>
-                                        <th scope="col">Cantidad</th>
-                                        <th scope="col">Precio</th>
-                                        <th scope="col" class="text-right"></th>
+                                        <th scope="col" width="120">Cantidad</th>
+                                        <th scope="col" width="120">Precio</th>
+                                        <th scope="col" class="text-right" width="200"></th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -44,7 +44,7 @@
                                                     @csrf
                                                     @method('PUT')
                                                     <input type="number" min="1" value="{{ $product->qty }}" name="qty"
-                                                        class="form-control col-3" onchange="this.form.submit()">
+                                                        class="form-control" onchange="this.form.submit()">
                                                 </form>
                                             </td>
                                             <td>
@@ -90,7 +90,7 @@
                                                     @csrf
                                                     @method('PUT')
                                                     <input type="number" min="1" value="{{ $product->qty }}" name="qty"
-                                                        class="form-control col-3" onchange="this.form.submit()">
+                                                        class="form-control" onchange="this.form.submit()">
                                                 </form>
                                             </td>
                                             <td>
@@ -109,114 +109,116 @@
                                 </tbody>
                             </table>
                             <div class="card-body border-top">
-                                <div class="d-flex justify-content-between">
-                                    <a href="/tienda" class="btn btn-light btn-sm"> <i class="fa fa-chevron-left"></i> Seguir
-                                        comprando
-                                    </a>
-                                    @if (Cart::instance('wishlist')->count() > 0)
-                                        <!-- Button trigger modal -->
-                                        <button type="button" class="btn btn-primary ml-5 btn-sm" data-toggle="modal"
-                                            data-target="#exampleModal">
-                                            Solicitar cotización
-                                        </button>
-                                </div>
-                                <!-- Modal -->
-                                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"
-                                    aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="exampleModalLabel">Informacion de contacto
-                                                </h5>
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-                                            <div class="modal-body">
-                                                <form action="{{ route('wishlist.sendCotizacion') }} " method="POST">
-                                                    @csrf
-                                                    <div class="form-group">
-                                                        <label for="name">Nombre completo</label>
-                                                        <input type="text" name="name" class="form-control">
-                                                        <label for="">Correo</label>
-                                                        <input type="text" name="email" class="form-control">
-                                                        <label for="">Telefono</label>
-                                                        <input type="text" name="phone" class="form-control">
-                                                        <label for="comment">Comentario(Opcional)</label>
-                                                        <textarea class="form-control" name="comment" rows="5"></textarea>
-                                                    </div>
-                                            </div>
-                                            <div class="modal-footer">
-                                                {{-- <button type="button" class="btn btn-default"
+                                <a href="/tienda" class="btn btn-light"> <i class="fa fa-chevron-left"></i> Seguir comprando
+                                </a>
+
+                                @if (Cart::instance('wishlist')->count() > 0)
+                                    <!-- Button trigger modal -->
+                                    <button type="button" class="btn btn-primary float-right" data-toggle="modal"
+                                        data-target="#exampleModal">
+                                        Solicitar cotización
+                                    </button>
+
+                                    <!-- Modal -->
+                                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog"
+                                        aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="exampleModalLabel">Informacion de contacto
+                                                    </h5>
+                                                    <button type="button" class="close" data-dismiss="modal"
+                                                        aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <form action="{{ route('wishlist.sendCotizacion') }} " method="POST">
+                                                        @csrf
+                                                        <div class="form-group">
+                                                            <label for="name">Nombre completo</label>
+                                                            <input type="text" name="name" class="form-control">
+                                                            <label for="">Correo</label>
+                                                            <input type="text" name="email" class="form-control">
+                                                            <label for="">Telefono</label>
+                                                            <input type="text" name="phone" class="form-control">
+                                                            <label for="comment">Comentario(Opcional)</label>
+                                                            <textarea class="form-control" name="comment"
+                                                                rows="5"></textarea>
+                                                        </div>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    {{-- <button type="button" class="btn btn-default"
                                                         data-dismiss="modal">Close</button> --}}
-                                                <button type="submit" class="btn btn-primary">Enviar</button>
+                                                    <button type="submit" class="btn btn-primary">Enviar</button>
+                                                </div>
+                                                </form>
                                             </div>
-                                            </form>
                                         </div>
                                     </div>
-                                </div>
-                @endif
-            </div>
-        </div> <!-- card.// -->
-        </main> <!-- col.// -->
-
-        <aside class="col-md-3">
-            @if (!session('coupon'))
-                <div class="card mb-3">
-                    <div class="card-body">
-                        <form action="{{ route('cart.applyCoupon') }}" method="post">
-                            @csrf
-                            <div class="form-group">
-                                <label>¿Tienes un cupón?</label>
-                                <div class="input-group">
-                                    <input type="text" class="form-control" name="coupon" placeholder="Código de cupón">
-                                    <span class="input-group-append">
-                                        <button type="submit" class="btn btn-primary">Aplicar</button>
-                                    </span>
-                                </div>
+                                @endif
                             </div>
-                        </form>
-                    </div> <!-- card-body.// -->
-                </div> <!-- card .// -->
-            @endif
-            <div class="card">
-                <div class="card-body">
-                    @if (Cart::instance('default')->count() > 0)
-                        <dl class="dlist-align">
-                            <h6>Total a pagar en linea</h6>
-                        </dl>
-                        <dl class="dlist-align">
-                            <dt>Precio total:</dt>
-                            @if (Cart::discount() < 0)
-                                <dd class="text-right">${{ Cart::instance('default')->subtotal() }} MXN</dd>
-                            @else
-                                @php
-                                    $subtotal = str_replace(',', '', Cart::instance('default')->subtotal());
-                                    $discount = str_replace(',', '', Cart::discount());
-                                    $subtotal = $subtotal + $discount;
-                                @endphp
-                                <dd class="text-right">${{ number_format($subtotal, 2) }} MXN</dd>
-                            @endif
-                        </dl>
-                        <dl class="dlist-align">
-                            <dt>Descuento:</dt>
-                            @if (Cart::discount() > 0)
-                                <dd class="text-right">
-                                    $-{{ Cart::discount(2, '.', ',') }} MXN
-                                    <small><a href="{{ route('cart.deleteCoupon') }}">Eliminar</a></small>
-                                </dd>
-                            @else
-                                <dd class="text-right">${{ Cart::discount(2, '.', ',') }} MXN</dd>
-                            @endif
-                        </dl>
-                        <dl class="dlist-align">
-                            <dt>Total:</dt>
-                            <dd class="text-right h5"><strong>${{ Cart::instance('default')->total() }}
-                                    MXN</strong></dd>
-                        </dl>
-                        <hr>
-                    @endif
-                    {{-- @if (Cart::instance('wishlist')->count() > 0)
+                        </div> <!-- card.// -->
+                    </main> <!-- col.// -->
+
+                    <aside class="col-md-3">
+                        @if (!session('coupon'))
+                            <div class="card mb-3">
+                                <div class="card-body">
+                                    <form action="{{ route('cart.applyCoupon') }}" method="post">
+                                        @csrf
+                                        <div class="form-group">
+                                            <label>¿Tienes un cupón?</label>
+                                            <div class="input-group">
+                                                <input type="text" class="form-control" name="coupon"
+                                                    placeholder="Código de cupón">
+                                                <span class="input-group-append">
+                                                    <button type="submit" class="btn btn-primary">Aplicar</button>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div> <!-- card-body.// -->
+                            </div> <!-- card .// -->
+                        @endif
+                        <div class="card">
+                            <div class="card-body">
+                                @if (Cart::instance('default')->count() > 0)
+                                    <dl class="dlist-align">
+                                        <h6>Total a pagar en linea</h6>
+                                    </dl>
+                                    <dl class="dlist-align">
+                                        <dt>Precio total:</dt>
+                                        @if (Cart::discount() < 0)
+                                            <dd class="text-right">${{ Cart::instance('default')->subtotal() }} MXN</dd>
+                                        @else
+                                            @php
+                                                $subtotal = str_replace(',', '', Cart::instance('default')->subtotal());
+                                                $discount = str_replace(',', '', Cart::discount());
+                                                $subtotal = $subtotal + $discount;
+                                            @endphp
+                                            <dd class="text-right">${{ number_format($subtotal, 2) }} MXN</dd>
+                                        @endif
+                                    </dl>
+                                    <dl class="dlist-align">
+                                        <dt>Descuento:</dt>
+                                        @if (Cart::discount() > 0)
+                                            <dd class="text-right">
+                                                $-{{ Cart::discount(2, '.', ',') }} MXN
+                                                <small><a href="{{ route('cart.deleteCoupon') }}">Eliminar</a></small>
+                                            </dd>
+                                        @else
+                                            <dd class="text-right">${{ Cart::discount(2, '.', ',') }} MXN</dd>
+                                        @endif
+                                    </dl>
+                                    <dl class="dlist-align">
+                                        <dt>Total:</dt>
+                                        <dd class="text-right h5"><strong>${{ Cart::instance('default')->total() }}
+                                                MXN</strong></dd>
+                                    </dl>
+                                    <hr>
+                                @endif
+                                {{-- @if (Cart::instance('wishlist')->count() > 0)
                                     <dl class="dlist-align">
                                         <h6>Cotizacion de productos del catálogo</h6>
                                     </dl>
@@ -231,24 +233,24 @@
                                     </dl>
                                     <hr>
                                 @endif --}}
-                    <p class="text-center mb-3">
-                        @if (Cart::instance('default')->count() > 0)
-                            <a href="{{ route('checkout.index') }}" class="btn btn-primary my-1 btn-block">
-                                Pagar con tarjeta
-                                <i class="far fa-credit-card"></i> </a>
-                            <a href="{{ route('checkout.cash') }}" class="btn btn-primary my-2 btn-block">
-                                Pagar en efectivo
-                                <i class="far fa-money-bill-alt"></i> </a>
-                        @endif
-                        <img class="mt-2" src="images/misc/payments.png" height="26">
-                    </p>
-                </div> <!-- card-body.// -->
-            </div> <!-- card .// -->
-        </aside> <!-- col.// -->
-    @else
-        <h3 class="mx-auto mt-4">No tienes ningun producto agregado al carrito y/o lista</h3>
-        @endif
-        </div>
+                                <p class="text-center mb-3">
+                                    @if (Cart::instance('default')->count() > 0)
+                                        <a href="{{ route('checkout.index') }}" class="btn btn-primary my-1 btn-block">
+                                            Pagar con tarjeta
+                                            <i class="far fa-credit-card"></i> </a>
+                                        <a href="{{ route('checkout.cash') }}" class="btn btn-primary my-2 btn-block">
+                                            Pagar en efectivo
+                                            <i class="far fa-money-bill-alt"></i> </a>
+                                    @endif
+                                    <img class="mt-2" src="images/misc/payments.png" height="26">
+                                </p>
+                            </div> <!-- card-body.// -->
+                        </div> <!-- card .// -->
+                    </aside> <!-- col.// -->
+                @else
+                    <h3 class="mx-auto mt-4">No tienes ningun producto agregado al carrito y/o lista</h3>
+                @endif
+            </div>
         </div> <!-- container .//  -->
     </section>
     <!-- ========================= SECTION CONTENT END// ========================= -->
