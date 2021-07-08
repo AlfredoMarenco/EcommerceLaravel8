ecc041760b19ce5e34afe7058275f1024ff48872
alvarbu <74802594+alvarbu@users.noreply.github.com>
2021-07-08 14:21:58 -0500

    cambio en productos del catalogo de description a descripcion

--- resources/views/bajce/catalog/product.blade.php
+++ resources/views/bajce/catalog/product.blade.php
@@ -98,7 +98,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <h5 class="title-description">Description</h5>
+                    <h5 class="title-description">Descripción</h5>
                     <p>
                         {!! $product->description !!}
                     </p>
