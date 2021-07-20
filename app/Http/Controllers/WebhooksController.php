27f33e1442a22dc3d255b2c7e34c181b2fab5464
marencoalf <alfredomarenco@boletea.com>
2021-07-20 10:59:43 -0500

    Activacion del webhook

--- app/Http/Controllers/WebhooksController.php
+++ app/Http/Controllers/WebhooksController.php
@@ -18,6 +18,7 @@ class WebhooksController extends Controller
     {
         /* $payment_id = $request->get('payment_id'); */
         $response = json_decode(file_get_contents('php://input'), true);
+        dump($response);
         Log::info($response);
         $payment_id = $response['id'];
         $response = Http::get("https://api.mercadopago.com/v1/payments/$payment_id" . "?access_token=TEST-1634125175479100-030323-b598123aafa36e3fb705cc8ce3d7f162-38750732");
