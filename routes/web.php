45e7c94c568b410c00aaeb5ec5bcf6c4e7ca5d93
marencoalf <alfredomarenco@boletea.com>
2021-07-20 11:04:32 -0500

    Activacion del webhook

--- routes/web.php
+++ routes/web.php
@@ -168,9 +168,9 @@ Route::get('/mailable', function () {
 
 
 Route::get('pay', function (Request $request) {
-    $payment_id = $request->get('payment_id');
+    /* $payment_id = $request->get('payment_id'); */
     $response = json_decode(file_get_contents('php://input'), true);
-    return $payment_id;
+    return $response->get();
     Log::info($response);
     $payment_id = $response['id'];
     $response = Http::get("https://api.mercadopago.com/v1/payments/$payment_id" . "?access_token=TEST-1634125175479100-030323-b598123aafa36e3fb705cc8ce3d7f162-38750732");
