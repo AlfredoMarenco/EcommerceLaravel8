e646cdcae857d1b8dfca2d59cfd5e61b2d747f7d
marencoalf <alfredomarenco@boletea.com>
2021-08-03 17:08:28 -0500

    Traduccion

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -56,7 +56,7 @@
     <!-- ============================ COMPONENT LOGIN   ================================= -->
     <div class="card mx-auto" style="max-width: 380px; margin-top:100px;">
         <div class="card-body">
-            <h4 class="card-title mb-4">Sign in</h4>
+            <h4 class="card-title mb-4">Iniciar sesión</h4>
             <form method="POST" action="{{ route('login') }}">
                 @csrf
                 <a href="{{ route('login.drive','facebook') }}" class="btn btn-facebook btn-block mb-2"> <i class="fab fa-facebook-f"></i> &nbsp Sign in with
@@ -71,20 +71,20 @@
                 </div> <!-- form-group// -->
 
                 <div class="form-group">
-                    <a href="{{ route('password.request') }}" class="float-right">Forgot password?</a>
+                    <a href="{{ route('password.request') }}" class="float-right">¿Olvidaste tu contraseña?</a>
                     <label class="float-left custom-control custom-checkbox"> <input type="checkbox"
                             class="custom-control-input" checked="">
-                        <div class="custom-control-label"> Remember </div>
+                        <div class="custom-control-label"> Recordarme </div>
                     </label>
                 </div> <!-- form-group form-check .// -->
                 <div class="form-group">
-                    <button type="submit" class="btn btn-dark btn-block"> Login </button>
+                    <button type="submit" class="btn btn-dark btn-block"> Ingresar </button>
                 </div> <!-- form-group// -->
             </form>
         </div> <!-- card-body.// -->
     </div> <!-- card .// -->
 
-    <p class="text-center mt-4">Don't have account? <a href="{{ route('register') }}">Sign up</a></p>
+    <p class="text-center mt-4">¿No tienes cuenta? <a href="{{ route('register') }}">Registrate</a></p>
     <br><br>
     <!-- ============================ COMPONENT LOGIN  END.// ================================= -->
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -51,23 +51,23 @@
     <div class="card mx-auto" style="max-width:520px; margin-top:80px;">
         <article class="card-body">
             <header class="mb-4">
-                <h4 class="card-title">Sign up</h4>
+                <h4 class="card-title">Registrarme</h4>
             </header>
             <form method="POST" action="{{ route('register') }}">
                 @csrf
                 <div class="form-row">
                     <div class="col form-group">
-                        <label>First name</label>
+                        <label>Nombre</label>
                         <input name="name" type="text" class="form-control" placeholder="" value="{{ old('name') }}">
                     </div>
                     <div class="col form-group">
-						<label>Last name</label>
+						<label>Apellido</label>
 					  	<input name="last_name" type="text" class="form-control" placeholder="" value="{{ old('last_name') }}">
 					</div>
                     <!-- form-group end.// -->
                 </div>
                 <div class="form-group">
-                    <label>Phone</label>
+                    <label>Telefono</label>
                     <input name="phone" type="phone" class="form-control" placeholder="" value="{{ old('phone') }}">
                 </div>
                 <!-- form-row end.// -->
@@ -81,25 +81,25 @@
                 <!-- form-row.// -->
                 <div class="form-row">
                     <div class="form-group col-md-6">
-                        <label>Create password</label>
+                        <label>Crear contraseña</label>
                         <input name="password" class="form-control" type="password">
                     </div> <!-- form-group end.// -->
                     <div class="form-group col-md-6">
-                        <label>Repeat password</label>
+                        <label>Repetir contraseña</label>
                         <input name="password_confirmation" class="form-control" type="password">
                     </div>
                     <!-- form-group end.// -->
                 </div>
                 <div class="form-group">
-                    <button type="submit" class="btn btn-dark btn-block"> Register </button>
+                    <button type="submit" class="btn btn-dark btn-block"> Registrarme </button>
                 </div>
                 <!-- form-group// -->
                 <div class="form-group">
                     <label class="custom-control custom-checkbox">
                         <input type="checkbox" class="custom-control-input"
                             checked="">
-                        <div class="custom-control-label text-dark"> I am agree with <a href="{{ route('login') }}">terms and
-                                contitions</a> </div>
+                        <div class="custom-control-label text-dark"> Estoy de acuerdo con los <a href="{{ route('login') }}">terminos y
+                                condiciones</a> </div>
                     </label>
                 </div>
                 <!-- form-group end.// -->
@@ -108,7 +108,7 @@
         <!-- card-body.// -->
     </div>
     <!-- card .// -->
-    <p class="text-center mt-4">Have an account? <a href="">Log In</a></p>
+    <p class="text-center mt-4">Ya tienes cuenta? <a href="">Iniciar sesión</a></p>
     <br><br>
     <!-- ============================ COMPONENT REGISTER  END.// ================================= -->
 
