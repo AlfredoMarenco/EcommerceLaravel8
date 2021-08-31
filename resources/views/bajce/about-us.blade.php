7e382ce184822bb74730fbd081ab5086ee8670dc
marencoalf <alfredomarenco@boletea.com>
2021-08-31 11:04:31 -0500

    Agregamos indicadores de las sucursuales

--- resources/views/bajce/about-us.blade.php
+++ resources/views/bajce/about-us.blade.php
@@ -276,11 +276,11 @@
                     </div>
                 </div>
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <a class="carousel-control-prev text-secondary h4" href="#carouselExampleIndicators" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"><</span>
                 <span class="sr-only">Previous</span>
             </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <a class="carousel-control-next text-secondary h4" href="#carouselExampleIndicators" role="button" data-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true">></span>
                 <span class="sr-only">Next</span>
             </a>
