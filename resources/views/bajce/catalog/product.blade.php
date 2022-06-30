7eddbb61c1ed19f757f1a84d443de836d9ee6a8b
unknown <alfredomarenco@boletea.com>
2022-06-30 12:54:39 -0500

    cambios

--- resources/views/bajce/catalog/product.blade.php
+++ resources/views/bajce/catalog/product.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.bajce')
 @section('title', $product->name)
 @section('titleFacebook', $product->name)
+@section('urlFacebook',URL::('/'))
 
 @section('imageFacebook', Storage::url($product->image->url))
 
